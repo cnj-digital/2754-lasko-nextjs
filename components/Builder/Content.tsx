eee3d74109f71ddc2fd015b2255f0d9aa91b9901
rickybrce <rikardo.komen@inet.hr>
2025-11-24 13:39:17 +0100

    content image padding

--- components/Builder/Content.tsx
+++ components/Builder/Content.tsx
@@ -80,7 +80,7 @@ export default function Content({
           src={asset.permalink}
           alt="content"
           className={cx(
-            "rounded-2xl",
+            "rounded-2xl my-10",
             variant?.value?.toLowerCase()?.trim() === "left" ? "lg:order-1" : (variant?.value?.toLowerCase()?.trim() === "center-asset-center-title" ? "" : "lg:order-2")
           )}
         />
