d08706f31f7266d6a41dd7fac545f237a74edeee
rickybrce <rikardo.komen@inet.hr>
2025-12-15 14:27:14 +0100

    script fix

--- components/Scripts.tsx
+++ components/Scripts.tsx
@@ -25,7 +25,11 @@ export default function Scripts() {
       )}
       {analytical && (
         <Script id="ms_clarity">
-          {`(function(c,l,a,r,i,t,y){ c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)}; t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i; y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y); })(window, document, "clarity", "script", “prlwan2bbt”);`}
+          {`(function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "prlwan2bbt");`}
         </Script>
       )}
       {marketing && (
